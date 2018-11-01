b26e993991fc871d5142e70475954123e07579a2
ddon <ddon@DDON.SC.ESILICON.com>
2018-11-01 16:53:59 +0700

    initial commit by DonDoan

---
+++ src/selenium_api/Topic_01_CheckEnvironment.java
@@ -0,0 +1,39 @@
+package selenium_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_01_CheckEnvironment {
+    WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		// Chrome
+		System.setProperty("webdriver.chrome.driver", ".\\lib\\chromedriver.exe");
+		driver = new ChromeDriver();
+
+		// Firefox
+//		driver = new FirefoxDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void TC_01_CheckTitle() {
+		String homePageTitle = driver.getTitle();
+		Assert.assertEquals(homePageTitle, "Google");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
